ad0f0da07d941c2e4219fba1b8c747e58fa3890a
Robert Espina <officialrobert.espina9627@gmail.com>
2026-03-10 19:12:40 +0800

    fix: init postgres + drizzle orm

--- packages/app/database/context.ts
+++ packages/app/database/context.ts
@@ -1,17 +1,19 @@
-import { AsyncLocalStorage } from 'node:async_hooks';
-
-import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
 import * as schema from './schema';
 
-export const DatabaseContext = new AsyncLocalStorage<
-  PostgresJsDatabase<typeof schema>
->();
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // 'postgresql://order_builder_user_postgres:order_builder_password_postgres@localhost:5432/order_builder',
+});
+
+export const db = drizzle(pool, { schema, casing: 'snake_case' });
 
 export function database() {
-  const db = DatabaseContext.getStore();
   if (!db) {
     throw new Error('DatabaseContext not set');
   }
+
   return db;
 }
